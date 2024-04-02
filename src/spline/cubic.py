b8c71487821f87f2535e843eba83e6a86331ba17
Sam Williams <samwilliams@usc.edu>
2024-04-02 12:55:51 -0700

    nonuniform looping splines on arbitrary indices

--- src/spline/cubic.py
+++ src/spline/cubic.py
@@ -8,50 +8,74 @@ class EndCondition(Enum):
     NATURAL = 1
     CLAMPED = 2
     CLOSED = 3
+    LOOPING = 4
+
+
+def _closed_or_looping(end_condition):
+    return end_condition == EndCondition.CLOSED or end_condition == EndCondition.LOOPING
 
 
 def solve_cubic_coeffs(t, x, end_condition=EndCondition.NATURAL, **kwargs):
     # t: T_max or (..., length)
     # x: (..., length, channels)
     if len(t.size()) == 0:
-        t = torch.linspace(0, t, x.shape[-2] if end_condition != EndCondition.CLOSED else x.shape[-2] + 1, device=x.device)
+        t = torch.linspace(0, t, x.shape[-2] if not _closed_or_looping(
+            end_condition) else x.shape[-2] + 1, device=x.device)
     # Check v_begin and v_end for CLAMPED end conditions
     if end_condition == EndCondition.CLAMPED:
         if "v_begin" not in kwargs.keys() or "v_end" not in kwargs.keys():
-            raise ValueError("missing v_begin or v_end for clamped end condition")
+            raise ValueError(
+                "missing v_begin or v_end for clamped end condition")
     # Check t_closed for CLOSED end conditions
-    if end_condition == EndCondition.CLOSED:
+    if _closed_or_looping(end_condition):
         if t.shape[0] != x.shape[-2] + 1:
-            raise ValueError("incorrect number of time points for closed cubic spline")
+            raise ValueError(
+                "incorrect number of time points for closed or looping cubic spline")
+        if end_condition == EndCondition.LOOPING:
+            if "loop_index" not in kwargs.keys():
+                raise ValueError(
+                    "missing loop_index for looping end condition")
 
     delta = (t[1:] - t[:-1]).unsqueeze(-1)
     delta_sq = delta ** 2
     delta_reciprocal = 1 / delta
     delta_reciprocal_sq = 1 / delta_sq
-    delta_plus1_index = x.shape[-2] if end_condition != EndCondition.CLOSED else -1
-    delta_plus0_index = -1 if end_condition != EndCondition.CLOSED else -2
+    delta_plus1_index = x.shape[-2] if not _closed_or_looping(
+        end_condition) else -1
+    delta_plus0_index = -1 if not _closed_or_looping(end_condition) else -2
 
-    # TODO: implement clamped and closed end conditions
     if end_condition == EndCondition.NATURAL:
         first = (3 * (x[..., 1, :] - x[..., 0, :])).unsqueeze(-2)
         last = (3 * (x[..., -1, :] - x[..., -2, :])).unsqueeze(-2)
     elif end_condition == EndCondition.CLAMPED:
         first = torch.ones_like(x[..., 0, :]).unsqueeze(-2) * kwargs["v_begin"]
         last = torch.ones_like(x[..., 0, :]).unsqueeze(-2) * kwargs["v_end"]
-    elif end_condition == EndCondition.CLOSED:
-        first = (3 * (x[..., 1, :] - x[..., -1, :])).unsqueeze(-2)
-        last = (3 * (x[..., 0, :] - x[..., -2, :])).unsqueeze(-2)
+    elif _closed_or_looping(end_condition):
+        loop_index = kwargs.get("loop_index", 0)
+        if loop_index == 0:
+            first = (3 * (x[..., 1, :] - x[..., -1, :])).unsqueeze(-2)
+            last = (3 * (x[..., 0, :] - x[..., -2, :])).unsqueeze(-2)
+        else:
+            # Natural condition for start of curve
+            first = (3 * (x[..., 1, :] - x[..., 0, :])).unsqueeze(-2)
+            # Equivalent to first condition for closed spline, C^2 continuity between last and loop
+            last = (3 * (x[..., loop_index + 1, :] -
+                    x[..., -1, :])).unsqueeze(-2)
 
-    # i - 1, i, i + 1
-    rhs = 3 * (x[..., 1:-1, :] - x[..., :-2, :]) * delta_reciprocal_sq[:delta_plus0_index] + 3 * (x[..., 2:, :] - x[..., 1:-1, :]) * delta_reciprocal_sq[1:delta_plus1_index]
+    rhs = 3 * (x[..., 1:-1, :] - x[..., :-2, :]) * delta_reciprocal_sq[:delta_plus0_index] + \
+        3 * (x[..., 2:, :] - x[..., 1:-1, :]) * \
+        delta_reciprocal_sq[1:delta_plus1_index]
     rhs = torch.cat([first, rhs, last], dim=-2)
-    # Setup tridiagonal
+    # Setup tridiagonal matrix
     tridiagonal = torch.zeros(
         (*x.shape[:-2], x.shape[-2], x.shape[-2]), device=x.device)
     diag = torch.arange(0, x.shape[-2], 1, dtype=int)
-    tridiagonal[..., diag[1:-1], diag[1:-1]] = 2 * delta_reciprocal[:delta_plus0_index, 0] + 2 * delta_reciprocal[1:delta_plus1_index, 0]
-    tridiagonal[..., diag[1:-1], diag[2:]] = delta_reciprocal[:delta_plus0_index, 0]
-    tridiagonal[..., diag[1:-1], diag[:-2]] = delta_reciprocal[1:delta_plus1_index, 0]
+    tridiagonal[..., diag[1:-1], diag[1:-1]] = 2 * delta_reciprocal[:delta_plus0_index,
+                                                                    0] + 2 * delta_reciprocal[1:delta_plus1_index, 0]
+    tridiagonal[..., diag[1:-1], diag[2:]
+                ] = delta_reciprocal[:delta_plus0_index, 0]
+    tridiagonal[..., diag[1:-1], diag[:-2]
+                ] = delta_reciprocal[1:delta_plus1_index, 0]
 
     # Fix first and last row
     if end_condition == EndCondition.NATURAL:
@@ -62,25 +86,42 @@ def solve_cubic_coeffs(t, x, end_condition=EndCondition.NATURAL, **kwargs):
     elif end_condition == EndCondition.CLAMPED:
         tridiagonal[..., 0, 0] = 1
         tridiagonal[..., -1, -1] = 1
-    elif end_condition == EndCondition.CLOSED:
-        tridiagonal[..., 0, 0] = 2 * delta_reciprocal[0, 0] + 2 * delta_reciprocal[-1, 0]
-        tridiagonal[..., 0, 1] = delta_reciprocal[0, 0]
-        tridiagonal[..., 0, -1] = delta_reciprocal[-1, 0]
-        tridiagonal[..., -1, -1] =  2 * delta_reciprocal[-1, 0] + 2 * delta_reciprocal[-2, 0]
-        tridiagonal[..., -1, -2] = delta_reciprocal[-2, 0]
-        tridiagonal[..., -1, 0] = delta_reciprocal[-1, 0]
+    elif _closed_or_looping(end_condition):
+        if loop_index == 0:
+            tridiagonal[..., 0, 0] = 2 * delta_reciprocal[0, 0] + \
+                2 * delta_reciprocal[-1, 0]
+            tridiagonal[..., 0, 1] = delta_reciprocal[0, 0]
+            tridiagonal[..., 0, -1] = delta_reciprocal[-1, 0]
+            tridiagonal[..., -1, -1] = 2 * \
+                delta_reciprocal[-1, 0] + 2 * delta_reciprocal[-2, 0]
+            tridiagonal[..., -1, -2] = delta_reciprocal[-2, 0]
+            tridiagonal[..., -1, 0] = delta_reciprocal[-1, 0]
+        else:
+            # Natural beginning
+            tridiagonal[..., 0, 0] = 2 * delta[0]
+            tridiagonal[..., 0, 1] = delta[0]
+            # Looping end
+            tridiagonal[..., -1, loop_index] = 2 * \
+                delta_reciprocal[loop_index, 0] + 2 * delta_reciprocal[-1, 0]
+            tridiagonal[..., -1, loop_index +
+                        1] = delta_reciprocal[loop_index, 0]
+            tridiagonal[..., -1, -1] = delta_reciprocal[-1, 0]
 
     # TODO:: efficient tridiagonal solver
     xdot = torch.linalg.solve(tridiagonal, rhs)
-    if end_condition == EndCondition.CLOSED:
+    if _closed_or_looping(end_condition):
         # Fixing dimensions to add the additional spline
-        x = torch.cat([x, x[..., 0, :].unsqueeze(-2)], dim=-2)
-        xdot = torch.cat([xdot, xdot[..., 0, :].unsqueeze(-2)], dim=-2)
+        x = torch.cat([x, x[..., loop_index, :].unsqueeze(-2)], dim=-2)
+        xdot = torch.cat(
+            [xdot, xdot[..., loop_index, :].unsqueeze(-2)], dim=-2)
 
     a = x[..., :-1, :]
     b = xdot[..., :-1, :]
-    c = 3 * (x[..., 1:, :] - x[..., :-1, :]) * delta_reciprocal_sq - xdot[..., 1:, :] * delta_reciprocal - 2 * xdot[..., :-1, :] * delta_reciprocal
-    d = 2 * (x[..., :-1, :] - x[..., 1:, :]) * delta_reciprocal * delta_reciprocal_sq + xdot[..., 1:, :] * delta_reciprocal_sq + xdot[..., :-1, :] * delta_reciprocal_sq
+    c = 3 * (x[..., 1:, :] - x[..., :-1, :]) * delta_reciprocal_sq - xdot[...,
+                                                                          1:, :] * delta_reciprocal - 2 * xdot[..., :-1, :] * delta_reciprocal
+    d = 2 * (x[..., :-1, :] - x[..., 1:, :]) * delta_reciprocal * delta_reciprocal_sq + \
+        xdot[..., 1:, :] * delta_reciprocal_sq + \
+        xdot[..., :-1, :] * delta_reciprocal_sq
 
     return t, a, b, c, d
 
--- tests/test_cubic.py
+++ tests/test_cubic.py
@@ -2,10 +2,11 @@ import unittest
 from spline import cubic
 import torch
 
+
 class CubicTestCase(unittest.TestCase):
     def setUp(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
+
     def test_uniform_coeffs(self):
         # 5 splines, 5 points (4 polynomials), 3 dim
         x = torch.rand([5, 5, 3], device=self.device) * 10
@@ -14,21 +15,25 @@ class CubicTestCase(unittest.TestCase):
         coeffs = cubic.solve_cubic_coeffs(t, x)
         spline = cubic.CubicSpline(coeffs)
         # Spline is interpolating
-        self.assertAlmostEqual(torch.linalg.norm(spline.position(times) - x).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            spline.position(times) - x).detach().cpu().item(), 0, 4)
 
         t, a, b, c, d = coeffs
         xtplus1 = a + b + c + d
         # Spline is continuous
-        self.assertAlmostEqual(torch.linalg.norm(xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
 
         xtdotplus1 = b + 2 * c + 3 * d
         # Spline is continuously differentiable
-        self.assertAlmostEqual(torch.linalg.norm(xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
 
         ciplus1 = c + 3 * d
         # Spline is twice continuously differentiable (for natural cubic splines)
-        self.assertAlmostEqual(torch.linalg.norm(ciplus1[..., :-1, :] - c[..., 1:, :]).detach().cpu().item(), 0, 4)
-    
+        self.assertAlmostEqual(torch.linalg.norm(
+            ciplus1[..., :-1, :] - c[..., 1:, :]).detach().cpu().item(), 0, 4)
+
     def test_nonuniform_coeffs(self):
         # 5 splines, 5 points (4 polynomials), 3 dim
         x = torch.rand([5, 5, 3], device=self.device) * 10
@@ -39,45 +44,111 @@ class CubicTestCase(unittest.TestCase):
 
         t, a, b, c, d = coeffs
         # Spline is interpolating
-        self.assertAlmostEqual(torch.linalg.norm(spline.position(t) - x).detach().cpu().item(), 0, 4)
-        
+        self.assertAlmostEqual(torch.linalg.norm(
+            spline.position(t) - x).detach().cpu().item(), 0, 4)
+
         xtplus1 = a + b * delta + c * delta ** 2 + d * delta ** 3
         # # Spline is continuous
-        self.assertAlmostEqual(torch.linalg.norm(xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
 
         xtdotplus1 = b + 2 * c * delta + 3 * d * delta ** 2
         # Spline is continuously differentiable
-        self.assertAlmostEqual(torch.linalg.norm(xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
 
     def test_clamped_coeffs(self):
         # 5 splines, 5 points (4 polynomials), 3 dim
         x = torch.rand([5, 5, 3], device=self.device) * 10
         t = torch.tensor([0., 1, 2, 3.5, 4.], device=self.device)
         with self.assertRaises(ValueError) as v:
-            coeffs = cubic.solve_cubic_coeffs(t, x, end_condition=cubic.EndCondition.CLAMPED)
-        coeffs = cubic.solve_cubic_coeffs(t, x, end_condition=cubic.EndCondition.CLAMPED, v_begin=2., v_end=-4.)
+            coeffs = cubic.solve_cubic_coeffs(
+                t, x, end_condition=cubic.EndCondition.CLAMPED)
+        coeffs = cubic.solve_cubic_coeffs(
+            t, x, end_condition=cubic.EndCondition.CLAMPED, v_begin=2., v_end=-4.)
         spline = cubic.CubicSpline(coeffs)
         delta = (t[1:] - t[:-1]).unsqueeze(-1)
 
         t, a, b, c, d = coeffs
         # Spline is interpolating
-        self.assertAlmostEqual(torch.linalg.norm(spline.position(t) - x).detach().cpu().item(), 0, 4)
-        
+        self.assertAlmostEqual(torch.linalg.norm(
+            spline.position(t) - x).detach().cpu().item(), 0, 4)
+
         xtplus1 = a + b * delta + c * delta ** 2 + d * delta ** 3
         # # Spline is continuous
-        self.assertAlmostEqual(torch.linalg.norm(xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
 
         xtdotplus1 = b + 2 * c * delta + 3 * d * delta ** 2
         # Spline is continuously differentiable
-        self.assertAlmostEqual(torch.linalg.norm(xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
 
     def test_closed_coeffs(self):
         # 5 splines, 5 points (4 polynomials), 3 dim
         x = torch.rand([5, 5, 3], device=self.device) * 10
         # t = torch.tensor([0., 1, 2, 3.5, 4.], device=self.device)
         t = torch.tensor(5., device=self.device)
-        coeffs = cubic.solve_cubic_coeffs(t, x, end_condition=cubic.EndCondition.CLOSED)
+        coeffs = cubic.solve_cubic_coeffs(
+            t, x, end_condition=cubic.EndCondition.CLOSED)
         spline = cubic.CubicSpline(coeffs)
         # Spline is interpolating
         t = torch.tensor([0., 1., 2., 3., 4.], device=self.device)
-        self.assertAlmostEqual(torch.linalg.norm(spline.position(t) - x).detach().cpu().item(), 0, 4)
+        self.assertAlmostEqual(torch.linalg.norm(
+            spline.position(t) - x).detach().cpu().item(), 0, 4)
+
+        t, a, b, c, d = coeffs
+        delta = (t[1:] - t[:-1]).unsqueeze(-1)
+        xtplus1 = a + b * delta + c * delta ** 2 + d * delta ** 3
+        # # Spline is continuous
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
+
+        xtdotplus1 = b + 2 * c * delta + 3 * d * delta ** 2
+        # Spline is continuously differentiable
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
+
+    def test_looping_coeffs(self):
+        # 5 splines, 5 points (4 polynomials), 3 dim
+        x = torch.rand([5, 5, 3], device=self.device) * 10
+        t = torch.tensor([0., 1, 2, 3.5, 4., 5.], device=self.device)
+
+        coeffs = cubic.solve_cubic_coeffs(
+            t, x, end_condition=cubic.EndCondition.LOOPING, loop_index=0)
+        closed_coeffs = cubic.solve_cubic_coeffs(
+            t, x, end_condition=cubic.EndCondition.CLOSED)
+
+        t, a, b, c, d = coeffs
+        t, c_a, c_b, c_c, c_d = closed_coeffs
+
+        def norm_diff(c1, c2): return torch.linalg.norm(
+            c1 - c2).detach().cpu().item()
+        # Looping and Closed splines should coincide when loop_index = 0
+        self.assertAlmostEqual(norm_diff(a, c_a), 0)
+        self.assertAlmostEqual(norm_diff(b, c_b), 0)
+        self.assertAlmostEqual(norm_diff(c, c_c), 0)
+        self.assertAlmostEqual(norm_diff(d, c_d), 0)
+
+        loop_index = 2
+        coeffs = cubic.solve_cubic_coeffs(
+            t, x, end_condition=cubic.EndCondition.LOOPING, loop_index=loop_index)
+
+        t, a, b, c, d = coeffs
+        delta = (t[1:] - t[:-1]).unsqueeze(-1)
+        xtplus1 = a + b * delta + c * delta ** 2 + d * delta ** 3
+        # # Spline is continuous
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtplus1[..., :-1, :] - a[..., 1:, :]).detach().cpu().item(), 0, 4)
+
+        # Spline loops on the correct index
+        self.assertAlmostEqual(norm_diff(xtplus1[..., -1, :], a[..., loop_index, :]), 0, 4)
+
+        xtdotplus1 = b + 2 * c * delta + 3 * d * delta ** 2
+        # Spline is continuously differentiable
+        self.assertAlmostEqual(torch.linalg.norm(
+            xtdotplus1[..., :-1, :] - b[..., 1:, :]).detach().cpu().item(), 0, 4)
+        
+        # Spline loop point is continuously differentiable
+        self.assertAlmostEqual(norm_diff(xtdotplus1[..., -1, :], b[..., loop_index, :]), 0, 4)
+
